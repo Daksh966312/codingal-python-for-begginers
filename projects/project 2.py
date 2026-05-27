11f5f31c713bd5cd32127af7c46f7179549577ed
Daksh Mirchandani <divyaunnati@gmail.com>
2026-05-27 15:38:10 +0530

    finished project 2

---
+++ projects/project 2.py
@@ -0,0 +1,10 @@
+print("Raj's freinds birthdays list")
+
+
+birthday1 = "Nilay- 14th of march 2012"
+birthday2 = "Ansh- 18th of september 2012"
+birthday3 = "Daksh- 1st of august 2012"
+birthday4 = "Vyan- 17th of June 2012"
+birthday5 = "Lakshya- 9th of august"
+
+print(birthday1, birthday2, birthday3, birthday4, birthday5)
