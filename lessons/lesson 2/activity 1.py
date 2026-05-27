89b8df0f77a3faee51f9b90ed7f2076dd54a0e50
Daksh Mirchandani <divyaunnati@gmail.com>
2026-05-27 11:16:26 +0530

    finished lesson 2 activity 1

---
+++ lessons/lesson 2/activity 1.py
@@ -0,0 +1,14 @@
+#today i'm attempting lesson 2 activities
+print("i ate mangoes")
+
+print(20)
+
+print("hello\n\nhola\n")
+
+print("python")
+
+print("my age is ", 13 * 1, "years old")
+
+print("welcome to", end="...")
+
+print("Navi Mumbai")
