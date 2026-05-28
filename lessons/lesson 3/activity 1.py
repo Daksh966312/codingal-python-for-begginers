b710f296845f4dda36cd5518f9ef7516b347b2ea
Daksh Mirchandani <divyaunnati@gmail.com>
2026-05-28 11:21:54 +0530

    finished lesson 3 activity 1

---
+++ lessons/lesson 3/activity 1.py
@@ -0,0 +1,23 @@
+# 1) Store an integer value in `a`.
+
+a = 123
+# 2) Print the datatype of `a` using `type()`.
+
+a = 123
+print("value of a =",type(a))
+# 3) Store a decimal (float) value in `b`.
+b=1.1414
+# 4) Print the datatype of `b` using `type()`.
+print("type of b =",type(b))
+
+# 5) Store a text (string) value in `c`.
+c = "mangoes"
+# 6) Print the datatype of `c` using `type()`.
+print("datatype of c =", type(c))
+# 7) Store a boolean value (True/False) in `d`.
+age=12
+is_adult = age >= 18
+print("value of is_adult =", is_adult)
+print("type of is_adult =",type(is_adult))
+# 8) Print the datatype of `d` using `type()`.# 1) Store an integer value in `a`.
+
