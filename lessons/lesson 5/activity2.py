d6efc0e7e549ccd88c9c520fdfafa758b82ec0b0
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-02 11:42:32 +0530

    finished lesson 5 activity2

---
+++ lessons/lesson 5/activity2.py
@@ -0,0 +1,10 @@
+cost_price = float(input("enter the original price- "))
+sale_price = float(input("enter the sale price- "))
+if sale_price > cost_price:
+    print("profit!")
+#print(sale_price - cost_price)
+
+else:
+    print("it is a loss")
+
+print(sale_price - cost_price)
