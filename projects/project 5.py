2e4a58b13e8e9b1fb895cbe09cbe9f3121718100
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-03 10:46:00 +0530

    finished project 5

---
+++ projects/project 5.py
@@ -0,0 +1,7 @@
+temperature = int(input("enter current tempersture- "))
+
+if temperature >= 30:
+    print("it is recommended to wear a light t-shirt, jeans, a cap, casual clothes")
+
+elif temperature <= 30:
+    print("it is recomended to wear light jacket, a hoodie or sweater")
