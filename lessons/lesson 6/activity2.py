ad5be4e58f74733b71ad93ab55b225a749aa1dae
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-03 11:56:18 +0530

    finished lesson 6 activity 2

---
+++ lessons/lesson 6/activity2.py
@@ -0,0 +1,13 @@
+a = "mango"
+b = "cocnut"
+
+print(a == b)
+
+print(not(a == b))
+
+num = int(input("enter any number-"))
+if not(num % 2 == 0):
+    print("its an odd number")
+
+else:
+    print("its an even number")
