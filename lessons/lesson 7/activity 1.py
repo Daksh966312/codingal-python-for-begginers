596c1ec70fe9a5085030fe924735aa1aa16ce720
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-04 11:30:03 +0530

    lesson 7 activity 1

---
+++ lessons/lesson 7/activity 1.py
@@ -0,0 +1,18 @@
+x = 20
+y = "mangoes"
+
+print(x == y)
+
+if x is y:
+    print("x and y have the same identity")
+
+else:
+    print("x and y dont have the same identity")
+
+#list
+
+x = [1, 2, 3, 4]
+y = [1, 2, 3, 4]
+
+print("x == y", x == y)
+print("x is y", x is y)
