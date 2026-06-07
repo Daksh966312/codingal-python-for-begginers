e810c1c5b2def4e4da433cce8a512b213f865f43
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-07 18:14:42 +0530

    finished lesson 8 activity 3

---
+++ lessons/lesson 8/activity3.py
@@ -0,0 +1,35 @@
+# 1) Store the given values:
+wrong_mean = 38
+# correct_mean = "x"
+incorrect_number = 36
+correct_number = 56
+total_obs = 40
+# `mean1` (wrong mean), `wrong_number`, `correct_number`, and `total_number`.
+
+# 2) Calculate the total sum using the wrong mean:
+sum = wrong_mean * total_obs - incorrect_number + correct_number
+correct_mean = sum / total_obs
+print(correct_mean)
+# - Multiply `mean1` by `total_number`
+
+# - Store it in `sum`
+
+# - Print the sum.
+
+# 3) Fix the sum to get the correct total:
+
+# - Remove the wrong number (subtract `wrong_number`)
+
+# - Add the correct number (add `correct_number`)
+
+# - Store the corrected total in `num2`
+
+# - Print the corrected sum.
+
+# 4) Find the correct mean:
+
+# - Divide `num2` by `total_number`
+
+# - Store it in `mean2`
+
+# - Print `mean2`.
