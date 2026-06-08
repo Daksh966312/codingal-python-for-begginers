1218a99ee7fcfc438fe64cb60789e62b34c851f5
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-08 09:58:27 +0530

    finished project 8

---
+++ projects/project 8.py
@@ -0,0 +1,27 @@
+x = 7
+y = 3
+
+# x = y
+# y = x
+# print(x,y)
+
+x,y = y,x
+print(x,y)
+
+a = 17
+b = 90
+c = 79
+
+a,b,c = b,c,a
+print(a,b,c)
+
+#use a temporary variable to temporarily store one of the values
+
+e = 17
+f = 19
+
+temp = e
+e = f
+f = temp 
+
+print(e,f)
