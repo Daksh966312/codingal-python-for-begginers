b04a67af68d16e583ef83f8707fec08be81f6b3b
Daksh Mirchandani <divyaunnati@gmail.com>
2026-06-11 11:58:46 +0530

    finishe dlesson 12 activity 1

---
+++ lessons/lesson 12/activity 1.py
@@ -0,0 +1,19 @@
+# 1) Ask the user to enter a word and store it in `string`.
+string = input("enter a word/phrase- ")
+# 2) Ask the user to enter a single character and store it in `char`.
+char = input("one single charector- ")
+# 3) Set `i` to 0.
+#    (This will be used as the index to move through the string.)
+# i = 0
+# 4) Set `count` to 0.
+#    (This will store how many times `char` appears.)
+count = 0
+# 5) While `i` is less than the length of `string`:
+#    a) Check if the character at position `i` in `string` is equal to `char`.
+#    b) If yes, increase `count` by 1.
+#    c) Increase `i` by 1 to move to the next character.
+for i in string:
+    if i == char:
+        count = count + 1
+# 6) After the loop, print how many times `char` occurred in `string` using `count`.
+print(count)
