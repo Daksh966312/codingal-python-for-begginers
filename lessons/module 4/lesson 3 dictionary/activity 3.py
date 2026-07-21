61ecad4460ddbd123594110e864e4afddd16c4cc
Daksh Mirchandani <divyaunnati@gmail.com>
2026-07-21 18:37:15 +0530

    finished m4 l3 a3

---
+++ lessons/module 4/lesson 3 dictionary/activity 3.py
@@ -0,0 +1,8 @@
+country_codes = {
+    "india" : "+91",
+    "Australia" : "+61",
+    "USA" : "+1"
+    
+}
+print(country_codes.get("Japan", "country code not found"))
+print(country_codes["USA"])
