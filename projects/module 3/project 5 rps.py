c1a1e2fd30d29194c0f873e4b6964c6e2773d694
Daksh Mirchandani <divyaunnati@gmail.com>
2026-07-26 15:12:11 +0530

    project rps

--- projects/module 3/project 5 rps.py
+++ projects/module 3/project 5 rps.py
@@ -1,2 +1,23 @@
 #create the rock paper sissors game using random.choice function
-choices = "rps" 
+import random
+choices = ("rock", "paper", "scissors")
+print(type(choices))
+
+player = input("rock paper or scissors (type quit to exit): ").lower()
+quit = "quit"
+if player == quit:
+    print("had fun!")
+    exit()
+comp = random.choice(choices)
+print(comp)
+
+if player == comp:
+    print("tie")
+elif player == "rock" and comp == "siccors":
+    print("player wins")
+elif player == "paper" and comp == "rock":
+    print("player wins")
+elif player == "scissors" and comp == "paper":
+    print("player wins")
+else:
+    print("computer wins")
