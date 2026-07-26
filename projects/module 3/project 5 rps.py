505eac03454b6001acaac775dcd8c04f38249f5a
Daksh Mirchandani <divyaunnati@gmail.com>
2026-07-26 15:15:45 +0530

    added while loop to rps

--- projects/module 3/project 5 rps.py
+++ projects/module 3/project 5 rps.py
@@ -2,22 +2,22 @@
 import random
 choices = ("rock", "paper", "scissors")
 print(type(choices))
+while True:
+    player = input("rock paper or scissors (type quit to exit): ").lower()
+    quit = "quit"
+    if player == quit:
+        print("had fun!")
+        exit()
+    comp = random.choice(choices)
+    print(comp)
 
-player = input("rock paper or scissors (type quit to exit): ").lower()
-quit = "quit"
-if player == quit:
-    print("had fun!")
-    exit()
-comp = random.choice(choices)
-print(comp)
-
-if player == comp:
-    print("tie")
-elif player == "rock" and comp == "siccors":
-    print("player wins")
-elif player == "paper" and comp == "rock":
-    print("player wins")
-elif player == "scissors" and comp == "paper":
-    print("player wins")
-else:
-    print("computer wins")
+    if player == comp:
+        print("tie")
+    elif player == "rock" and comp == "siccors":
+        print("player wins")
+    elif player == "paper" and comp == "rock":
+        print("player wins")
+    elif player == "scissors" and comp == "paper":
+        print("player wins")
+    else:
+        print("computer wins")
