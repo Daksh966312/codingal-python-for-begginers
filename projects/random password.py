57b9887abc2606cd3ab97c765fcc7c045826cbae
Daksh Mirchandani <divyaunnati@gmail.com>
2026-08-02 19:33:28 +0530

    finished random passwords

---
+++ projects/random password.py
@@ -0,0 +1,18 @@
+import string 
+import random
+
+length = int(input("enter password: "))
+
+characters = string.ascii_letters
+string.ascii_uppercase + string.digits
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+password_list = list(password)
+random.shuffle(password_list)
+passwrd = "".join(password_list)
+
+print(password)
